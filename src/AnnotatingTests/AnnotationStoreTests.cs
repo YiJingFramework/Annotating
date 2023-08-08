9bd68972d255ad275f6a6fd3f153b7dd4d773c42
yueyinqiu <lujiaqi2003@126.com>
2023-08-08 13:57:03 +0800

    aw

---
+++ src/AnnotatingTests/AnnotationStoreTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YiJingFramework.Annotating.Tests;
+
+[TestClass()]
+public class AnnotationStoreTests
+{
+    [TestMethod()]
+    public void AnnotationStoreTest()
+    {
+        var store = new AnnotationStore()
+        {
+            Title = "Sample Store"
+        };
+
+        store.Tags.Add("Tag1");
+        store.Tags.Add("Tag2");
+        store.Tags.Add("Tag3");
+
+        var namingGroup = store.AddGroup(title: "Gua Name", comment: "Names of the Guas");
+        namingGroup.AddEntry("111", "Qian");
+        namingGroup.AddEntry("000", "Kun");
+
+        var serialized = store.SerializeToJsonString();
+        Assert.AreEqual(
+            "{\"n\":\"Sample Store\",\"t\":[\"Tag1\",\"Tag2\",\"Tag3\"]" +
+            ",\"g\":[{\"t\":\"Gua Name\",\"e\":[{\"t\":\"111\",\"c\":\"Qian\"}" +
+            ",{\"t\":\"000\",\"c\":\"Kun\"}],\"c\":\"Names of the Guas\"}]}",
+            serialized);
+
+        var d = AnnotationStore.DeserializeFromJsonString(serialized);
+        var serialized2 = d?.SerializeToJsonString();
+        Assert.AreEqual(serialized, serialized2);
+    }
+}
